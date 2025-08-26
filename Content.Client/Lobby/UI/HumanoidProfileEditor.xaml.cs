875a651f5c17cf9248d85139edf19523d1cc6684
MarkerWicker <markerWicker@proton.me>
2025-08-26 16:28:55 -0600

    create custom selector for traits

--- Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
+++ Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
@@ -156,6 +156,7 @@
 using System.IO;
 using System.Linq;
 using System.Numerics;
+using Content.Client._Omu.Lobby.Ui.Roles; // Omustation - Remake EE Traits System - change TraitPreferenceSelector for TraitRequirementsSelector
 using Content.Client.Humanoid;
 using Content.Client.Lobby.UI.Loadouts;
 using Content.Client.Lobby.UI.Roles;
@@ -737,50 +738,42 @@ namespace Content.Client.Lobby.UI
                     });
                 }
 
-                List<RequirementsSelector?> selectors = new(); // Omustation - Remake EE Traits System - change TraitPreferenceSelector for RequirementsSelector
+                List<TraitRequirementsSelector?> selectors = new(); // Omustation - Remake EE Traits System - change TraitPreferenceSelector for RequirementsSelector
                 var selectionCount = 0;
 
                 foreach (var traitProto in categoryTraits)
                 {
                     var trait = _prototypeManager.Index<TraitPrototype>(traitProto);
                     // begin Omustation - Remake EE Traits System - change TraitPreferenceSelector for RequirementsSelector
-                    var selector = new RequirementsSelector();
+                    var selector = new TraitRequirementsSelector();
 
-                    var selectorOptions = new[] { ("On", 0), ("Off", 1) };
                     var selectorName = trait.Cost != 0 ? Loc.GetString(trait.Name) + " [" + trait.Cost + "]" : Loc.GetString(trait.Name);
                     var selectorDescription = Loc.GetString(trait.Description != null ? trait.Description : "");
 
-                    selector.Setup(selectorOptions, selectorName, 200, selectorDescription);
+                    selector.Setup(selectorName, selectorDescription, trait.TraitPointCost);
 
-                    selector.Select(Profile?.TraitPreferences.Contains(trait.ID) == true ? 0 : 1);
+                    selector.Preference = Profile?.TraitPreferences.Contains(trait.ID) == true ? true : false;
 
                     // requirements should be checked after the profile's traits are loaded, so that traits with unmet requirements can be removed from the character.
                     if (!_requirements.CheckRoleRequirements(trait.Requirements, (HumanoidCharacterProfile?) _preferencesManager.Preferences?.SelectedCharacter, out var reason))
                     {
                         selector.LockRequirements(reason);
-
-                        Profile = Profile?.WithoutTraitPreference(trait.ID, _prototypeManager);
-                        // NOTE: there is a bug in the statement above. If you select a trait that has (for example) a species requirement using a species that's allowed to have that trait,
-                        // and then you select a species that isn't allowed to have that trait, the character will still have that trait until you press the save button a second time.
-                        // If you don't hit the save button a second time, the character keeps that trait. This isn't a major issue, as the server will still prevent a player
-                        // with a restricted trait from spawning, but it is one that I'd lke to fix if possible. Do note that this also happens with the job RequirementsSelectors, so this
-                        // bug should be fixed upstream (on wizden)
                     }
                     else
                     {
                         selector.UnlockRequirements();
                     }
 
-                    if (selector.Selected == 0)
+                    if (selector.Preference)
                     {
                         selectionCount += trait.Cost;
                         _selectedTraitCount++;
                         _selectedTraitPointCount -= trait.TraitPointCost;
                     }
 
-                    selector.OnSelected += preference =>
+                    selector.PreferenceChanged += preference =>
                     {
-                        if (preference == 0 &&
+                        if (preference &&
                         _selectedTraitCount < _maxTraits && // make sure the player isn't selecting more traits than they're allowed
                         _selectedTraitPointCount - trait.TraitPointCost >= 0) // make sure that the player isn't spending more trait points than they're allowed
                         {
