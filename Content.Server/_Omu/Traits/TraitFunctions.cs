e3c02d2c201c9e81c2cddc317d6ec33d90fe417b
MarkerWicker <markerWicker@proton.me>
2025-09-02 22:25:14 -0600

    add Bodybuilder trait

--- Content.Server/_Omu/Traits/TraitFunctions.cs
+++ Content.Server/_Omu/Traits/TraitFunctions.cs
@@ -1,6 +1,8 @@
 using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization.Manager;
 using Content.Shared._Omu.Traits;
+using Robust.Shared.Physics.Systems;
+using Robust.Shared.Physics;
 
 namespace Content.Server._Omu.Traits;
 
@@ -19,5 +21,30 @@ public sealed partial class TraitRemoveComponent : TraitFunction
     {
         foreach (var (name, _) in ComponentsToRemove)
             entityManager.RemoveComponentDeferred(uid, factory.GetComponent(name).GetType());
+    }
+}
+
+public sealed partial class TraitModifyDensity : TraitFunction
+{
+    [DataField, AlwaysPushInheritance]
+    public float DensityModifier;
+
+    [DataField, AlwaysPushInheritance]
+    public bool Multiply = false;
+
+    public override void OnPlayerSpawn(EntityUid uid,
+        IComponentFactory factory,
+        IEntityManager entityManager,
+        ISerializationManager serializationManager)
+    {
+        var physicsSystem = entityManager.System<SharedPhysicsSystem>();
+        if (!entityManager.TryGetComponent<FixturesComponent>(uid, out var fixturesComponent)
+            || fixturesComponent.Fixtures.Count is 0)
+            return;
+
+        var fixture = fixturesComponent.Fixtures["fix1"]; // As of writing, fix1 is the fixture used for practically everything.
+        var newDensity = Multiply ? fixture.Density * DensityModifier : fixture.Density + DensityModifier;
+        physicsSystem.SetDensity(uid, "fix1", fixture, newDensity);
+        // SetDensity handles the Dirty.
     }
 }
--- Resources/Locale/en-US/_Omu/traits/traits.ftl
+++ Resources/Locale/en-US/_Omu/traits/traits.ftl
@@ -1,3 +1,8 @@
+trait-bodybuilder-name = Bodybuilder
+trait-bodybuilder-desc =
+    Through extensive training or body modification, you have achieved the pinnacle of physique.
+    This trait increases the density of your body by 55, making dragging / picking things up easier.
+
 trait-moth-flight-name = True Flight
 trait-moth-flight-desc =
     Unlike other mothpeople, your body is light enough and your wings are strong enough to be able to fly under normal gravity.
