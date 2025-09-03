0dbd617459a0ca37138fd449ce626a219002e00c
MarkerWicker <markerWicker@proton.me>
2025-09-02 23:16:42 -0600

    added claws and natural weapons removal

--- Content.Server/_Omu/Traits/TraitFunctions.cs
+++ Content.Server/_Omu/Traits/TraitFunctions.cs
@@ -3,12 +3,18 @@ using Robust.Shared.Serialization.Manager;
 using Content.Shared._Omu.Traits;
 using Robust.Shared.Physics.Systems;
 using Robust.Shared.Physics;
+using Content.Shared.Damage;
+using Content.Shared.Weapons.Melee;
+using Robust.Shared.Audio;
 
 namespace Content.Server._Omu.Traits;
 
 /// <summary>
-/// Used for traits that remove a component upon a player spawning in.
+///     Used for traits that remove a component upon a player spawning in.
 /// </summary>
+/// <remarks>
+///     taken from EE. 
+/// </remarks>
 public sealed partial class TraitRemoveComponent : TraitFunction
 {
     [DataField, AlwaysPushInheritance]
@@ -24,6 +30,9 @@ public sealed partial class TraitRemoveComponent : TraitFunction
     }
 }
 
+/// <remarks>
+///     taken from EE. 
+/// </remarks>
 public sealed partial class TraitModifyDensity : TraitFunction
 {
     [DataField, AlwaysPushInheritance]
@@ -46,5 +55,88 @@ public sealed partial class TraitModifyDensity : TraitFunction
         var newDensity = Multiply ? fixture.Density * DensityModifier : fixture.Density + DensityModifier;
         physicsSystem.SetDensity(uid, "fix1", fixture, newDensity);
         // SetDensity handles the Dirty.
+    }
+}
+
+/// <summary>
+///     Used for traits that modify unarmed damage on MeleeWeaponComponent.
+/// </summary>
+/// <remarks>
+///     taken from EE. 
+/// </remarks>
+public sealed partial class TraitModifyUnarmed : TraitFunction
+{
+    // <summary>
+    //     The sound played on hitting targets.
+    // </summary>
+    [DataField, AlwaysPushInheritance]
+    public SoundSpecifier? HitSound;
+
+    // <summary>
+    //     The animation to play on hit, for both light and power attacks.
+    // </summary>
+    [DataField, AlwaysPushInheritance]
+    public EntProtoId? Animation;
+
+    // <summary>
+    //     Whether to set the power attack animation to be the same as the light attack.
+    // </summary>
+    [DataField, AlwaysPushInheritance]
+    public bool HeavyAnimationFromLight = true;
+
+    // <summary>
+    //     The damage values of unarmed damage.
+    // </summary>
+    [DataField, AlwaysPushInheritance]
+    public DamageSpecifier? Damage;
+
+    // <summary>
+    //     Additional damage added to the existing damage.
+    // </summary>
+    [DataField, AlwaysPushInheritance]
+    public DamageSpecifier? FlatDamageIncrease;
+
+    // <summary>
+    //     What to multiply the melee weapon range by.
+    // </summary>
+    [DataField, AlwaysPushInheritance]
+    public float? RangeModifier;
+
+    // <summary>
+    //     What to multiply the attack rate by.
+    // </summary>
+    [DataField, AlwaysPushInheritance]
+    public float? AttackRateModifier;
+
+    public override void OnPlayerSpawn(EntityUid uid,
+        IComponentFactory factory,
+        IEntityManager entityManager,
+        ISerializationManager serializationManager)
+    {
+        if (!entityManager.TryGetComponent<MeleeWeaponComponent>(uid, out var melee))
+            return;
+
+        if (HitSound != null)
+            melee.HitSound = HitSound;
+
+        if (Animation != null)
+            melee.Animation = Animation.Value;
+
+        if (HeavyAnimationFromLight)
+            melee.WideAnimation = melee.Animation;
+
+        if (Damage != null)
+            melee.Damage = Damage;
+
+        if (FlatDamageIncrease != null)
+            melee.Damage += FlatDamageIncrease;
+
+        if (RangeModifier != null)
+            melee.Range *= RangeModifier.Value;
+
+        if (AttackRateModifier != null)
+            melee.AttackRate *= AttackRateModifier.Value;
+
+        entityManager.Dirty(uid, melee);
     }
 }
--- Resources/Locale/en-US/_Omu/traits/traits.ftl
+++ Resources/Locale/en-US/_Omu/traits/traits.ftl
@@ -3,11 +3,26 @@ trait-bodybuilder-desc =
     Through extensive training or body modification, you have achieved the pinnacle of physique.
     This trait increases the density of your body by 55, making dragging / picking things up easier.
 
+trait-claws-name = Claws
+trait-claws-desc = 
+    Your fingertips have been replaced with sharp claws.
+    These could come from gene modifications, vatgrown implants,
+    or even hard plastic retractable claws incorpoated into a prosthetic limb.
+    Your unarmed melee attacks deal Slashing damage instead of the standard damage type for your species.
+    This has no effect on damage dealt with any form of armed melee.
+
 trait-moth-flight-name = True Flight
 trait-moth-flight-desc =
     Unlike other mothpeople, your body is light enough and your wings are strong enough to be able to fly under normal gravity.
     Flight is faster than running; however, it is strenuous and quickly drains your stamina.
     By selecting this trait, you lose your ability to dash. 
+
+trait-weapons-removal-name = Natural Weapons Removal
+trait-weapons-removal-desc = 
+    Whatever "Natural Weapons" your species are normally born with have been surgically removed.
+    This could have been done to better fit in with terran space stations, or as a cosmetic choice.
+    As a result, your unarmed attacks deal Blunt damage instead of the standard damage type for your species.
+    This has no effect on damage dealt with any form of armed melee.
 
 trait-singer-name = Singer
 trait-singer-desc =
